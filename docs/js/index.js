5d06f02a3628fa05ebe136d79f4ad40e8749fbe6
HARA Masahiro <hara@claris.local>
2021-05-04 16:45:38 +0900

    change js and css

---
+++ docs/js/index.js
@@ -0,0 +1,1 @@
+// show hostname of this serverconst showHostName = ()=> {    const hostValue = location.hostname;        const hostArea = document.getElementById("main-hostname");     hostArea.innerHTML = `${hostValue}`;}// register event listenerwindow.addEventListener('load', ()=> {  showHostName();});
